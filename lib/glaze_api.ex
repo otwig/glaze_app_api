46c5f2001169052db532e3ceed95b3453a1142ad
Olivia Hartwig <oliviahartwig@gmail.com>
2022-03-07 15:23:47 -0500

    adds poison decimal encoder

--- lib/glaze_api.ex
+++ lib/glaze_api.ex
@@ -6,4 +6,12 @@ defmodule GlazeApi do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  defimpl Poison.Encoder, for: Decimal do
+    def encode(decimal, options) do
+      decimal
+      |> Decimal.to_string(:xsd)
+      |> Poison.Encoder.encode(options)
+    end
+  end
 end
