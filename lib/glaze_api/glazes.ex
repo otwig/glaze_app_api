19e0a8043c84aa11c1d4e6e648d2101f02676e9e
Olivia Hartwig <oliviahartwig@gmail.com>
2022-03-10 16:24:16 -0500

    comments out functions i dont think are needed thanks for ja resource

--- lib/glaze_api/glazes.ex
+++ lib/glaze_api/glazes.ex
@@ -1,118 +1,118 @@
-defmodule GlazeApi.Glazes do
-  import Ecto.Query, warn: false
+# defmodule GlazeApi.Glazes do
+#   import Ecto.Query, warn: false
 
-  alias GlazeApi.Repo
-  alias GlazeApi.{Glaze, Image, Ingredient}
+#   alias GlazeApi.Repo
+#   alias GlazeApi.{Glaze, Image, Ingredient}
 
-  def list_glazes do
-    Repo.all(Glaze)
-    |> Repo.preload([:images, :ingredients])
-  end
+#   def list_glazes do
+#     Repo.all(Glaze)
+#     |> Repo.preload([:images, :ingredients])
+#   end
 
-  def get_glaze!(id) do
-    Glaze
-    |> Repo.get!(id)
-    |> Repo.preload([:ingredients, :images])
-  end
+#   def get_glaze!(id) do
+#     Glaze
+#     |> Repo.get!(id)
+#     |> Repo.preload([:ingredients, :images])
+#   end
 
-  def one_glaze_with_all(id) do
-    id
-    |> glaze_with_all()
-    |> Repo.one!()
-  end
+#   def one_glaze_with_all(id) do
+#     id
+#     |> glaze_with_all()
+#     |> Repo.one!()
+#   end
 
-  def glaze_with_all(id) do
-    from g in Glaze,
-    where: g.id == ^id,
-    preload: ^[images: images_query(id), ingredients: ingredients_query(id)]
-  end
-
-
-#   # --------------------------- #
-
-  def get_glaze_with_images(id) do
-    id
-    |> glaze_with_images()
-    |> Repo.one!()
-  end
-
-  def glaze_with_images(id) do
-    from g in Glaze,
-    where: g.id == ^id,
-    preload: ^[images: images_query(id)]
-    # join: im in Image,
-    # on: im.glaze_id == g.id
-  end
-
-  def images_query(id) do
-    from i in Image,
-    where: i.glaze_id == ^id,
-    select: i.img_url
-  end
+#   def glaze_with_all(id) do
+#     from g in Glaze,
+#     where: g.id == ^id,
+#     preload: ^[images: images_query(id), ingredients: ingredients_query(id)]
+#   end
 
 
-# # ----------------------------- #
-  def get_glaze_with_ingredients(id) do
-    id
-    |> glaze_with_ingredients()
-    |> Repo.one!()
-  end
+# #   # --------------------------- #
 
-  def glaze_with_ingredients(id) do
-    from g in Glaze,
-    where: g.id == ^id,
-    preload: ^[ingredients: ingredients_query(id)]
-    # join: im in Image,
-    # on: im.glaze_id == g.id
-  end
+#   def get_glaze_with_images(id) do
+#     id
+#     |> glaze_with_images()
+#     |> Repo.one!()
+#   end
 
-  defp ingredients_query(id) do
-    from ing in Ingredient,
-    where: ing.glaze_id == ^id,
-    select: [ing.name, ing.amount]
-  end
+#   def glaze_with_images(id) do
+#     from g in Glaze,
+#     where: g.id == ^id,
+#     preload: ^[images: images_query(id)]
+#     # join: im in Image,
+#     # on: im.glaze_id == g.id
+#   end
 
-# # ----------------------------- #
-# # ----------------------------- #
-  # def create_glaze(attrs \\ %{}) do
-  #   %Glaze{}
-  #   |> Glaze.changeset(attrs)
-  #   |> Repo.insert()
-  #   |> case do
-  #     {:ok, %Glaze{} = glaze} -> {:ok, Repo.preload(glaze, :images, :ingredients)}
-  #     error -> error
-  #   end
-  # end
+#   def images_query(id) do
+#     from i in Image,
+#     where: i.glaze_id == ^id,
+#     select: i.img_url
+#   end
 
 
-  # def update_glaze(%Glaze{} = glaze, attrs) do
-  #   glaze
-  #   |> Glaze.changeset(attrs)
-  #   |> Repo.update()
-  # end
+# # # ----------------------------- #
+#   def get_glaze_with_ingredients(id) do
+#     id
+#     |> glaze_with_ingredients()
+#     |> Repo.one!()
+#   end
 
-  # def delete_glaze(%Glaze{} = glaze) do
-  #   Repo.delete(glaze)
-  # end
+#   def glaze_with_ingredients(id) do
+#     from g in Glaze,
+#     where: g.id == ^id,
+#     preload: ^[ingredients: ingredients_query(id)]
+#     # join: im in Image,
+#     # on: im.glaze_id == g.id
+#   end
 
-  # def change_glaze(%Glaze{} = glaze, attrs \\ %{}) do
-  #   Glaze.changeset(glaze, attrs)
-  # end
+#   defp ingredients_query(id) do
+#     from ing in Ingredient,
+#     where: ing.glaze_id == ^id,
+#     select: [ing.name, ing.amount]
+#   end
+
+# # # ----------------------------- #
+# # # ----------------------------- #
+#   # def create_glaze(attrs \\ %{}) do
+#   #   %Glaze{}
+#   #   |> Glaze.changeset(attrs)
+#   #   |> Repo.insert()
+#   #   |> case do
+#   #     {:ok, %Glaze{} = glaze} -> {:ok, Repo.preload(glaze, :images, :ingredients)}
+#   #     error -> error
+#   #   end
+#   # end
 
 
-  # def create_image(%Glaze{} = glaze, attrs \\ %{}) do
-  #   glaze
-  #   |> Ecto.build_assoc(:images)
-  #   |> Option.changeset(attrs)
-  #   |> Repo.insert()
-  # end
+#   # def update_glaze(%Glaze{} = glaze, attrs) do
+#   #   glaze
+#   #   |> Glaze.changeset(attrs)
+#   #   |> Repo.update()
+#   # end
 
-  #   def create_ingredient(%Glaze{} = glaze, attrs \\ %{}) do
-  #   glaze
-  #   |> Ecto.build_assoc(:ingredients)
-  #   |> Option.changeset(attrs)
-  #   |> Repo.insert()
-  # end
+#   # def delete_glaze(%Glaze{} = glaze) do
+#   #   Repo.delete(glaze)
+#   # end
+
+#   # def change_glaze(%Glaze{} = glaze, attrs \\ %{}) do
+#   #   Glaze.changeset(glaze, attrs)
+#   # end
 
 
-end
+#   # def create_image(%Glaze{} = glaze, attrs \\ %{}) do
+#   #   glaze
+#   #   |> Ecto.build_assoc(:images)
+#   #   |> Option.changeset(attrs)
+#   #   |> Repo.insert()
+#   # end
+
+#   #   def create_ingredient(%Glaze{} = glaze, attrs \\ %{}) do
+#   #   glaze
+#   #   |> Ecto.build_assoc(:ingredients)
+#   #   |> Option.changeset(attrs)
+#   #   |> Repo.insert()
+#   # end
+
+
+# end
--- lib/glaze_api_web/controllers/glaze_controller.ex
+++ lib/glaze_api_web/controllers/glaze_controller.ex
@@ -1,7 +1,7 @@
 defmodule GlazeApiWeb.GlazeController do
   use GlazeApiWeb, :controller
 
-  alias GlazeApi.Glaze
+  alias GlazeApiWeb.Glaze
   alias GlazeApi.Repo
 
   use JaResource
@@ -15,37 +15,33 @@ defmodule GlazeApiWeb.GlazeController do
   #   where(Glaze, id: ^id)
   # end
 
-  ## this works in iex but i think the above is what i need
-  def get_glaze(id) do
-    Glaze |> Repo.get(id)
-  end
+  # ## this works in iex but i think the above is what i need
+  # def get_glaze(id) do
+  #   Glaze |> Repo.get(id)
+  # end
 
-  defimpl Poison.Encoder, for: Decimal do
-    def encode(decimal, options) do
-      Decimal.to_string(decimal, :normal)
-      |> Poison.Encoder.encode(options)
-    end
-  end
+  # defimpl Poison.Encoder, for: Decimal do
+  #   def encode(decimal, options) do
+  #     Decimal.to_string(decimal, :normal)
+  #     |> Poison.Encoder.encode(options)
+  #   end
+  # end
 
-  def create_glaze(attrs \\ %{}) do
-    %Glaze{}
-    |> Glaze.changeset(attrs)
-    |> Ecto.Changeset.cast_assoc(:ingredient, with: &Ingredient.changeset/2)
-    |> Repo.insert()
-  end
+  # def create_glaze(attrs \\ %{}) do
+  #   %Glaze{}
+  #   |> Glaze.changeset(attrs)
+  #   |> Ecto.Changeset.cast_assoc(:ingredient, with: &Ingredient.changeset/2)
+  #   |> Repo.insert()
+  # end
 
-  def update_glaze(%Glaze{} = glaze, attrs \\ %{}) do
-    glaze
-    |> Glaze.changeset(attrs)
-    |> Repo.update()
-  end
+  # def update_glaze(%Glaze{} = glaze, attrs \\ %{}) do
+  #   glaze
+  #   |> Glaze.changeset(attrs)
+  #   |> Repo.update()
+  # end
 
-  def delete_glaze(id) when is_integer(id) do
-    %Glaze{id: id}
-    |> Repo.delete()
-  end
-
-  # def handle_show(conn, id) do
-  #   Repo.get_by(Glaze, id: id)
+  # def delete_glaze(id) when is_integer(id) do
+  #   %Glaze{id: id}
+  #   |> Repo.delete()
   # end
 end
